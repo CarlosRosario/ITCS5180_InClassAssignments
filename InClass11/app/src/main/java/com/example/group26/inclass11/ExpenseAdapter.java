fca287e0423553ecf5007f6aeacc4346166ee88e
Carlos Rosario-Desktop <carlos.rosario1990@gmail.com>
2016-04-12 01:49:54 -0400

    [hell]firebase project

---
+++ InClass11/app/src/main/java/com/example/group26/inclass11/ExpenseAdapter.java
@@ -0,0 +1,52 @@
+package com.example.group26.inclass11;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Carlos on 4/11/2016.
+ */
+public class ExpenseAdapter extends ArrayAdapter<Expense> {
+
+    List<Expense> mData;
+    Context mContext;
+    int mResource;
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //return super.getView(position, convertView, parent);
+
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(mResource, parent, false);
+        }
+
+        Expense expense = mData.get(position);
+
+        // Set venue name
+        TextView venueNameTextView = (TextView)convertView.findViewById(R.id.categoryNameCustomTextView);
+        venueNameTextView.setText(expense.getExpenseName());
+
+        // Set category name
+        TextView categoryNameTextView = (TextView)convertView.findViewById(R.id.dollarAmountCustomTextView);
+        categoryNameTextView.setText("$" + expense.getAmount());
+
+
+        return convertView;
+
+    }
+
+    public ExpenseAdapter(Context context, int resource, List<Expense> objects){
+        super(context, resource, objects);
+        this.mData = objects;
+        this.mContext = context;
+        this.mResource = resource;
+    }
+}
